5c603fc3d9e9ff076efdf0498c7f9b20cb8d7e9a
User <User@PREPOD-CAB1>
2023-11-24 02:48:45 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp6/HumanJsonExtension.cs
@@ -0,0 +1,40 @@
+﻿// методы расширения
+// Extension
+// Можно создавать методы, которые смогут выполняться над
+// объектами классов, которыми мы не управляем (не можем изменить)
+
+// например, допустим, что класс Human объявлен в отдельной
+// библиотеке и недоступен нам для изменения
+// но мы хотим добавить метод, который можно вызвать на объекте
+// класса Human
+//метод расширения создается в публичном статичном классе с любым именем
+//сам метод тоже объявляется как публичный и статичный
+//первый аргумент метода включает в себя слово this и тип класса
+//для которого создается расширение
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Test
+{
+    public static class HumanJsonExtension
+    {
+        public static string GetJson(this Human human)
+        {
+            return JsonSerializer.Serialize(human);
+        }
+
+        public static string GetXml(this Human human)
+        {
+            string result = null;
+            XmlSerializer xml = new XmlSerializer(typeof(Human));
+            using (var ms = new MemoryStream())
+            using (var sr = new StreamReader(ms))
+            {
+                xml.Serialize(ms, human);
+                ms.Position = 0;
+                result = sr.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,14 @@
+﻿// методы расширения
+// Extension
+// Можно создавать методы, которые смогут выполняться над
+// объектами классов, которыми мы не управляем (не можем изменить)
+
+// например, допустим, что класс Human объявлен в отдельной
+// библиотеке и недоступен нам для изменения
+// но мы хотим добавить метод, который можно вызвать на объекте
+// класса Human
+using Test;
+Human human = new Human { LastName = "Петров", FirstName = "Сергей", PatronymicName = "Венеаминович" };
+Console.WriteLine(human.GetFio());
+Console.WriteLine(human.GetJson());
+Console.WriteLine(human.GetXml());
